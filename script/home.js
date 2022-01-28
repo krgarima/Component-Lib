6681665c20e20b1c471602c195a63e7958425433
Garima <kumargarima59@gmail.com>
2022-01-28 13:17:29 +0530

    updated Home page
    created navbar and sidebar

---
+++ script/home.js
@@ -0,0 +1,19 @@
+const hamburgerMenuModal = document.querySelector("#hamburgerMenuModal");
+const modalDrawer = document.querySelector("#modalDrawer");
+const closeModalDrawer = document.querySelector("#modalDrawerMainBody");
+
+function openModalDrawer() {
+  modalDrawer.style.visibility === "visible"
+    ? (modalDrawer.style.visibility = "hidden")
+    : (modalDrawer.style.visibility = "visible");
+  closeModalDrawer.style.backgroundColor = "rgb(145, 145, 145)";
+}
+
+function closeModalDrawerFn() {
+  if (modalDrawer.style.visibility === "visible")
+    modalDrawer.style.visibility = "hidden";
+  closeModalDrawer.style.backgroundColor = "white";
+}
+
+hamburgerMenuModal.addEventListener("click", openModalDrawer);
+closeModalDrawer.addEventListener("mousedown", closeModalDrawerFn);
