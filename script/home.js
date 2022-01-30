9d0474ed58c58191246307f5cce576323ad0a2ba
Garima <kumargarima59@gmail.com>
2022-01-30 16:50:54 +0530

    fix: minor issues

--- script/home.js
+++ script/home.js
@@ -1,18 +1,18 @@
 const hamburgerMenuModal = document.querySelector("#hamburgerMenuModal");
 const modalDrawer = document.querySelector("#modalDrawer");
 const closeModalDrawer = document.querySelector("#modalDrawerMainBody");
+const closeDrawer = document.querySelector("#close");
 
 function openModalDrawer() {
   modalDrawer.style.visibility === "visible"
     ? (modalDrawer.style.visibility = "hidden")
     : (modalDrawer.style.visibility = "visible");
-  closeModalDrawer.style.backgroundColor = "rgb(145, 145, 145)";
 }
 
 function closeModalDrawerFn() {
   if (modalDrawer.style.visibility === "visible")
     modalDrawer.style.visibility = "hidden";
-  closeModalDrawer.style.backgroundColor = "white";
 }
 
 hamburgerMenuModal.addEventListener("click", openModalDrawer);
+closeDrawer.addEventListener("click", closeModalDrawerFn);
